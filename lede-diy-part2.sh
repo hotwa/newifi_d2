a1003b5d4d38f6b824a852bdb47510a30307ca81
hotwa <ze.ga@qq.com>
2022-07-10 21:49:20 +0800

    change lan to 10.1.10.1

--- lede-diy-part2.sh
+++ lede-diy-part2.sh
@@ -10,7 +10,7 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.8.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/10.1.10.1/g' package/base-files/files/bin/config_generate
 
 #修正连接数（by ベ七秒鱼ベ）
 sed -i '/customized in this file/a net.netfilter.nf_conntrack_max=165535' package/base-files/files/etc/sysctl.conf
