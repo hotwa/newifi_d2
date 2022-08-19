99de4c86a5404730a864e9897472dd958dde1ea4
曾令宇 <lingyuzeng@Lingyus-MacBook-Air.local>
2022-08-19 18:54:53 +0800

    decrease

--- lede-diy-part1.sh
+++ lede-diy-part1.sh
@@ -23,22 +23,5 @@ sed -i '$a src-git coolsnowwolf_packages https://github.com/coolsnowwolf/package
 sed -i '$a src-git coolsnowwolf_luci https://github.com/coolsnowwolf/luci' feeds.conf.default
 #sed -i '$a src-git kiddin9 https://github.com/kiddin9/openwrt-packages' feeds.conf.default
 
-# add dts file
-mv jdc_re-cp-02.dts ./target/linux/ramips/dts/jdc_re-cp-02.dts
-# ./target/linux/ramips/image/mt7621.mk
-cat << EOF >> target/linux/ramips/image/mt7621.mk 
-define Device/jdc_re-cp-02
-  $(Device/uimage-lzma-loader)
-  IMAGE_SIZE := 16064k  
-  DEVICE_VENDOR := JDCloud
-  DEVICE_MODEL := re-cp-02
-  DEVICE_COMPAT_VERSION := 1.0
-  DEVICE_PACKAGES := kmod-mt7603e kmod-mt76x2e kmod-usb3 kmod-usb-ledtrig-usbport luci-app-mtwifi \
-        kmod-sdhci kmod-sdhci-mt7620 -wpad-openssl
-endef
-TARGET_DEVICES += jdc_re-cp-02
-EOF
-# ./target/linux/ramips/base-files/lib/upgrade/platform.sh
-# 最后生成的固件在目录
 
-# ./bin/targets/ramips/mt7621/
+ß
